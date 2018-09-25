3d287887dc9c6b8130d13bc123bbe4f05a711577
Benjamin Huang <bhuang@rms.com>
2018-09-24 19:39:00 -0700

    file upload

--- excel-upload-api/src/main/java/excelupload/RESTApp.java
+++ excel-upload-api/src/main/java/excelupload/RESTApp.java
@@ -2,7 +2,14 @@ package excelupload;
 
 import excelupload.resources.FileService;
 import io.dropwizard.Application;
+import io.dropwizard.forms.MultiPartBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class RESTApp extends Application<RESTServiceConfig> {
 
@@ -10,8 +17,28 @@ public class RESTApp extends Application<RESTServiceConfig> {
         new RESTApp().run(args);
     }
 
+
+    @Override
+    public void initialize(Bootstrap<RESTServiceConfig> bootstrap) {
+        bootstrap.addBundle(new MultiPartBundle());
+    }
+
     @Override
     public void run(RESTServiceConfig config, Environment env) {
+
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                env.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+
         // Register services
         env.jersey().register(new FileService());
 
--- excel-upload-api/src/main/java/excelupload/resources/FileService.java
+++ excel-upload-api/src/main/java/excelupload/resources/FileService.java
@@ -1,13 +1,15 @@
 package excelupload.resources;
 
 import com.codahale.metrics.annotation.Timed;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
+import java.io.*;
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/file")
 public class FileService {
@@ -18,4 +20,45 @@ public class FileService {
         return "Hello";
     }
 
+
+    @POST
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+        String uploadedFileLocation = "./upload" + fileDetail.getFileName();
+
+        // save it
+        writeToFile(uploadedInputStream, uploadedFileLocation);
+
+        String output = "File uploaded to : " + uploadedFileLocation;
+
+        //
+        return Response.status(200).entity(output).build();
+    }
+
+    // save uploaded file to new location
+    private void writeToFile(InputStream uploadedInputStream,
+                             String uploadedFileLocation) {
+
+        try {
+            OutputStream out = new FileOutputStream(new File(
+                    uploadedFileLocation));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            out = new FileOutputStream(new File(uploadedFileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
 }
